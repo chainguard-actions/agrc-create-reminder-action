3bc98c75357219119dfed012ca69ff24c2cbff9e
actionchain <actionchain@chainguard.dev>
2026-06-17 12:11:26 +0000

    Update agrc/create-reminder-action@v1.1.24

---
+++ src/index.test.ts
@@ -0,0 +1,194 @@
+import type { IssueCommentCreatedEvent } from '@octokit/webhooks-types';
+import { describe, expect, test } from 'vitest';
+import issueContext from '../test/fixtures/issue_comment_payload.json' with { type: 'json' };
+import { addReminderToBody, getReminder } from './utilities.js';
+
+describe('getReminder', () => {
+  test('can parse context', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(issueContext as IssueCommentCreatedEvent, REFERENCE_DATE);
+
+    expect(reminder).toEqual({
+      who: 'Codertocat',
+      when: new Date(2017, 6, 6, 9, 0, 0, 0),
+      what: 'do something',
+    });
+  });
+  test('returns null if not a slash command', () => {
+    const reminder = getReminder({
+      ...issueContext,
+      comment: {
+        body: 'not a command',
+      },
+    } as IssueCommentCreatedEvent);
+
+    expect(reminder).toBeNull();
+  });
+  test('returns null if the command is not remind', () => {
+    const reminder = getReminder({
+      ...issueContext,
+      comment: {
+        body: '/not a command',
+      },
+    } as IssueCommentCreatedEvent);
+
+    expect(reminder).toBeNull();
+  });
+  test('can parse multiline comments', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(
+      {
+        ...issueContext,
+        comment: {
+          body: 'This is a test\r\n/remind me to do this in one day\r\nwith some text',
+        },
+      } as IssueCommentCreatedEvent,
+      REFERENCE_DATE,
+    );
+
+    expect(reminder).toEqual({
+      who: 'Codertocat',
+      when: new Date(2017, 6, 6, 9, 0, 0, 0),
+      what: 'do this',
+    });
+  });
+  test('does not parse reminder within line', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(
+      {
+        ...issueContext,
+        comment: {
+          body: 'This is a test: /remind me to do this in one day',
+        },
+      } as IssueCommentCreatedEvent,
+      REFERENCE_DATE,
+    );
+
+    expect(reminder).toBeNull();
+  });
+  test('skips reminder in quote', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(
+      {
+        ...issueContext,
+        comment: {
+          body: 'This is a test\r\n> with some text\r\n> /remind me to do this in one day',
+        },
+      } as IssueCommentCreatedEvent,
+      REFERENCE_DATE,
+    );
+
+    expect(reminder).toBeNull();
+  });
+  test('skips reminder in code block', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(
+      {
+        ...issueContext,
+        comment: {
+          body: 'This is a test\r\n```\r\n/remind me to do this in one day\r\n```',
+        },
+      } as IssueCommentCreatedEvent,
+      REFERENCE_DATE,
+    );
+
+    expect(reminder).toBeNull();
+  });
+  test('skips reminder in code block 2', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(
+      {
+        ...issueContext,
+        comment: {
+          body: 'This is a test\r\n```python\r\n/remind me to do this in one day\r\n```',
+        },
+      } as IssueCommentCreatedEvent,
+      REFERENCE_DATE,
+    );
+
+    expect(reminder).toBeNull();
+  });
+  test('skips reminder in inline code', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(
+      {
+        ...issueContext,
+        comment: {
+          body: 'This is a test: `/remind me to do this in one day`',
+        },
+      } as IssueCommentCreatedEvent,
+      REFERENCE_DATE,
+    );
+
+    expect(reminder).toBeNull();
+  });
+  test('works again after code block', () => {
+    const REFERENCE_DATE = new Date(2017, 6, 5, 4, 3, 2, 0);
+    const reminder = getReminder(
+      {
+        ...issueContext,
+        comment: {
+          body: 'This is a test\r\n```\r\ncode here!\r\n```\r\n/remind me to do this in one day',
+        },
+      } as IssueCommentCreatedEvent,
+      REFERENCE_DATE,
+    );
+
+    expect(reminder).toEqual({
+      who: 'Codertocat',
+      when: new Date(2017, 6, 6, 9, 0, 0, 0),
+      what: 'do this',
+    });
+  });
+});
+
+describe('addReminderToBody', () => {
+  test('adds a reminder to an issue body', () => {
+    const reminder = {
+      who: '@hello',
+      what: 'do it',
+      when: new Date(Date.UTC(2003, 0, 2, 0, 0, 0, 0)),
+    };
+    const body = addReminderToBody('this is the body', reminder);
+
+    const expected = `this is the body
+
+<!-- bot: {"reminders":[{"id":1,"who":"@hello","what":"do it","when":"2003-01-02T00:00:00.000Z"}]} -->`;
+
+    expect(body).toEqual(expected);
+  });
+  test('adds a reminder to an issue body with an existing reminder list', () => {
+    const reminder = {
+      who: '@someone',
+      what: 'to something',
+      when: new Date(Date.UTC(2021, 0, 1, 0, 0, 0, 0)),
+    };
+    const existing = `
+      this is the body
+
+<!-- bot: {"reminders":[{"id":1,"who":"@hello","what":"do it","when":"2003-01-02"}]} -->
+    `;
+    const expected = `
+      this is the body
+
+<!-- bot: {"reminders":[{"id":1,"who":"@hello","what":"do it","when":"2003-01-02"},{"id":2,"who":"@someone","what":"to something","when":"2021-01-01T00:00:00.000Z"}]} -->
+    `;
+    const result = addReminderToBody(existing, reminder);
+
+    expect(result).toEqual(expected);
+  });
+  test('adds a reminder to an issue body that is empty', () => {
+    const reminder = {
+      who: '@hello',
+      what: 'do it',
+      when: new Date(Date.UTC(2003, 0, 2, 0, 0, 0, 0)),
+    };
+    const body = addReminderToBody(null, reminder);
+
+    const expected = `
+
+<!-- bot: {"reminders":[{"id":1,"who":"@hello","what":"do it","when":"2003-01-02T00:00:00.000Z"}]} -->`;
+
+    expect(body).toEqual(expected);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,110 @@
+import { endGroup, getInput, info, setFailed, startGroup } from '@actions/core';
+import { getOctokit, context as ghContext } from '@actions/github';
+import type { IssueCommentCreatedOrEditedEvent } from './types.js';
+import { addReminderToBody, getReminder } from './utilities.js';
+const LABEL = 'reminder';
+
+type Octokit = ReturnType<typeof getOctokit>;
+
+function getIssueProps(context: IssueCommentCreatedOrEditedEvent) {
+  const inputOwner = getInput('repositoryOwner');
+  const inputRepository = getInput('repository');
+  let repo: string;
+  if (inputRepository) {
+    const parts = inputRepository.split('/');
+    if (parts.length !== 2) {
+      throw new Error(`Invalid repository input: ${inputRepository}`);
+    }
+    repo = parts[1]!;
+  } else {
+    repo = context.repository.name;
+  }
+
+  return {
+    owner: inputOwner ?? context.repository.owner.login,
+    repo: repo,
+    issue_number: context.issue.number,
+  };
+}
+
+function createComment(octokit: Octokit, context: IssueCommentCreatedOrEditedEvent, body: string) {
+  return octokit.rest.issues.createComment({
+    ...getIssueProps(context),
+    body,
+  });
+}
+
+function updateIssue(octokit: Octokit, context: IssueCommentCreatedOrEditedEvent, reminder: Reminder) {
+  const body = addReminderToBody(context.issue.body || null, reminder);
+
+  return octokit.rest.issues.update({
+    ...getIssueProps(context),
+    body,
+  });
+}
+
+async function run() {
+  const testContextPath = getInput('testContextPath', { required: false });
+  let testContext: IssueCommentCreatedOrEditedEvent | null = null;
+  if (testContextPath) {
+    info('running in test mode');
+
+    try {
+      testContext = await import(testContextPath);
+    } catch (error) {
+      setFailed(`failed to import testContextPath (${testContextPath}): ${error}`);
+
+      return;
+    }
+  }
+  const context = testContext || (ghContext.payload as IssueCommentCreatedOrEditedEvent);
+  const octokit = getOctokit(getInput('repoToken', { required: true }));
+  let reminder: Reminder | null = null;
+
+  try {
+    startGroup('parsing reminder');
+    reminder = getReminder(context);
+
+    info(JSON.stringify(reminder, null, 1));
+
+    if (!reminder) {
+      info('no reminder found');
+      return;
+    }
+    endGroup();
+  } catch (error) {
+    startGroup('create error comment');
+    await createComment(
+      octokit,
+      context,
+      `@${context.sender.login} we had trouble parsing your reminder. Try:\n\n\`/remind me [what] [when]\``,
+    );
+    endGroup();
+
+    setFailed(error instanceof Error ? error.message : String(error));
+
+    return;
+  }
+
+  startGroup('add label');
+  info(JSON.stringify(getIssueProps(context), null, 1));
+  await octokit.rest.issues.addLabels({
+    ...getIssueProps(context),
+    labels: [LABEL],
+  });
+  endGroup();
+
+  startGroup('update issue');
+  await updateIssue(octokit, context, reminder);
+  endGroup();
+
+  startGroup('add reminder comment');
+  await createComment(
+    octokit,
+    context,
+    `@${context.sender.login} set a reminder for **${reminder.when.toISOString().split('T')[0]}**`,
+  );
+  endGroup();
+}
+
+run();
